9d30168b7fb048f394e1a8621ef0c014baa05d6d
Nicolas Nascimento Maso <nicolas.maso@seventh.com.br>
2023-05-10 17:10:13 +0000

    app.module.ts edited online with Bitbucket

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,19 @@ import { HttpModule } from './infra/http/http.module'
 import { MessagingModule } from './infra/messaging/messaging.module'
 import { ConfigModule } from '@nestjs/config'
 import { ScheduleModule } from '@nestjs/schedule'
+import { ServeStaticModule } from '@nestjs/serve-static'
+import { join } from 'path'
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     ScheduleModule.forRoot(),
     HttpModule,
-    MessagingModule
+    MessagingModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'coverage', 'lcov-report'),
+      renderPath: '/tests'
+    }),
   ],
 })
 export class AppModule {}
