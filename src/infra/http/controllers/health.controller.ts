855b285eebeb392ff1b5d8143608beeb874d0fad
Nicolas Nascimento Maso <nicolas.maso@seventh.com.br>
2023-05-10 17:06:57 +0000

    health.controller.ts edited online with Bitbucket

--- src/infra/http/controllers/health.controller.ts
+++ src/infra/http/controllers/health.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get } from '@nestjs/common'
+import { Can } from '../configs/can.decorator'
 
 @Controller('health')
 export class HealthController {
   @Get()
+  @Can()
   async handler(): Promise<any> {
     return {
       message: `DGC Example running in environment ${process.env.ENV_MODE}`
